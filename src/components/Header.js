1776c59d215145ba6755b9b974aea248ddf04bc9
sigitfrank <sigittt53@gmail.com>
2021-04-10 12:52:51 +0700

    fix links section on profile page

--- src/components/Header.js
+++ src/components/Header.js
@@ -42,7 +42,7 @@ function Navbar() {
   }
   return (<>
     <header className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link className="navbar-brand" to="/">-</Link>
+      <Link className="navbar-brand" to="/">Logo</Link>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaGlobeAsia, FaTwitter, FaFacebookF, FaInstagram } from 'react-icons/fa'
+import { FiGlobe, FiTwitter, FiFacebook, FiInstagram } from 'react-icons/fi'
 import '../css/profile.css'
 function ProfileScreen() {
     return (<div className="row">
@@ -9,7 +9,7 @@ function ProfileScreen() {
                     <div className="avatar-profile">
                         <img src="https://cybercampus.um-surabaya.ac.id/themes/bak/assets/admin/pages/media/profile/profile_user.jpg" alt="profile" className="avatar-img" />
                     </div>
-                    <div className="profile-detail text-center mt-3">
+                    <div className="short-profile text-center mt-3">
                         <p>John Doe</p>
                         <p>Full Stack Developer</p>
                         <p>Bay Area, San Francisco, CA</p>
@@ -24,16 +24,138 @@ function ProfileScreen() {
                 <div className="card-body">
                     <ul>
                         <li>
-                            <FaGlobeAsia className="link-icons" />  Website <span className="social-media">sigifrank.github.io</span>
+                            <div className="row">
+                                <div className="col-1">
+                                    <FiGlobe className="link-icons" />
+                                </div>
+                                <div className="col-10">
+                                    <span className="social-media">sigifrank.github.io</span>
+                                </div>
+                            </div>
                         </li>
                         <li>
-                            <FaTwitter className="link-icons" /> Twitter <span className="social-media">sigitfrank</span>
+                            <div className="row">
+                                <div className="col-1">
+                                    <FiTwitter className="link-icons" />
+                                </div>
+                                <div className="col-10">
+                                    <span className="social-media">sigitfrank</span>
+                                </div>
+                            </div>
                         </li>
                         <li>
-                            <FaInstagram className="link-icons" /> Instagram <span className="social-media">sigitfrank</span>
+                            <div className="row">
+                                <div className="col-1">
+                                    <FiInstagram className="link-icons" />
+                                </div>
+                                <div className="col-10">
+                                    <span className="social-media">sigitfrank</span>
+                                </div>
+                            </div>
                         </li>
                         <li>
-                            <FaFacebookF className="link-icons" /> Facebook <span className="social-media">sigittw</span>
+                            <div className="row">
+                                <div className="col-1">
+                                    <FiFacebook className="link-icons" />
+                                </div>
+                                <div className="col-10">
+                                    <span className="social-media">sigitfrank</span>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div className="col-md-7">
+            <div className="card profile-details mt-3">
+                <div className="card-body">
+                    <ul>
+                        <li>
+                            <div className="row">
+                                <div className="col-5">
+                                    Nama Lengkap
+                                </div>
+                                <div className="col-7">
+                                    <span className="social-media">Sigit Tunggul Waskito</span>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="row">
+                                <div className="col-5">
+                                    Email
+                                </div>
+                                <div className="col-7">
+                                    <span className="social-media">sigit@gmail.com</span>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="row">
+                                <div className="col-5">
+                                    Nomor HP
+                                </div>
+                                <div className="col-7">
+                                    <span className="social-media">082178306189</span>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="row">
+                                <div className="col-5">
+                                    Alamat
+                                </div>
+                                <div className="col-7">
+                                    <span className="social-media">Jalan Douglas D8 No 8 Beringin Raya</span>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div className="card profile-details mt-3">
+                <div className="card-body">
+                    <ul>
+                        <li>
+                            <div className="row">
+                                <div className="col-5">
+                                    Nama Lengkap
+                                </div>
+                                <div className="col-7">
+                                    <span className="social-media">Sigit Tunggul Waskito</span>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="row">
+                                <div className="col-5">
+                                    Email
+                                </div>
+                                <div className="col-7">
+                                    <span className="social-media">sigit@gmail.com</span>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="row">
+                                <div className="col-5">
+                                    Nomor HP
+                                </div>
+                                <div className="col-7">
+                                    <span className="social-media">082178306189</span>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="row">
+                                <div className="col-5">
+                                    Alamat
+                                </div>
+                                <div className="col-7">
+                                    <span className="social-media">Jalan Douglas D8 No 8 Beringin Raya</span>
+                                </div>
+                            </div>
                         </li>
                     </ul>
                 </div>
